67b95fe8be5f87f67982d99ce9dca33fe70bd69d
Jayvon Whetstone <jayvonwhetstone@Jayvons-Mac-mini.local>
2024-03-05 10:07:41 -0600

    pushing block07 workshop to github

---
+++ block07-workshop.js
@@ -0,0 +1,16 @@
+//Pseudocode: 1. Display message to user about combination
+//2. Assign the 3 variables
+//3. Create the dialog box to display the vault codes.
+//4. Push to github repo
+//5. Submit link
+
+//Creating the first string for the user.
+let nice = document.getElementById("str");
+str.innerHTML="You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//Creating the variables.
+const num1 = 5*2
+const num2 = 30+10
+const num3 = 40-1
+
+//Displaying the vault codes.
+alert(num1 + " " + num2 + " " + num3);
